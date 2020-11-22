6ef47e23c32558e2f6de6c3997a9d5409e36e461
Dio-Dao <59789616+Dio-Dao@users.noreply.github.com>
2020-11-22 18:21:43 +0300

    commit (#1)

---
+++ 01/Sergei Fediukov/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section className="about">
+            <div className='about_container'>
+                <div className='about_img'>
+                    <div className='some_img'>Some img</div>
+
+                </div>
+                <div className='about_text'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit iure quam, provident rem accusamus impedit veritatis expedita totam sapiente deserunt!
+                </div>
+            </div>
+        </section>
+    )
+}
+export default About
---
+++ 01/Sergei Fediukov/src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <section className="slider">
+            <div className="container">
+                <div>Some slides</div>
+            </div>
+        </section>
+    )
+}
+export default Header
