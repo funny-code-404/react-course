fe1ea7739b2f49cd35fa16dc46a156d15a62773d
Metakirin <abbadarel@gmail.com>
2020-11-26 19:08:50 +0300

    [03] add hw

---
+++ 03/klim/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./App.scss";
+
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
