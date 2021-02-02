59151f0891ca354b2f6605383aae67f72ec597b8
NikitaKhadnevich <70269922+NikitaKhadnevich@users.noreply.github.com>
2021-02-02 18:55:19 +0300

    [HOMEWORK-2] Fixed  tags  in DataSlogan

--- 01/Nikita Khadnevich/src/components/Midblock/DataSlogan/dataSlogan.js
+++ 01/Nikita Khadnevich/src/components/Midblock/DataSlogan/dataSlogan.js
@@ -1,7 +1,8 @@
 
 const DataSlogan = () => {
    return (
-      'We need the tonic of wildness...At the same time that we are earnest to explore and learn all things, we require that all things be mysterious and unexplorable, that land and sea be indefinitely wild, unsurveyed and unfathomed by us because unfathomable. We can never have enough of nature'
+      <>'We need the tonic of wildness...At the same time that we are earnest to explore and learn all things, we require that all things be mysterious and unexplorable, that land and sea be indefinitely wild, unsurveyed and unfathomed by us because unfathomable. We can never have enough of nature'
+      </>
    )
 }
 export default DataSlogan
