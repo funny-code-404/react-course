4cfc1c3231be93b7d42a23306e1fed2e6799c535
NikitaKhadnevich <70269922+NikitaKhadnevich@users.noreply.github.com>
2021-02-15 09:29:22 +0300

    [HOMEWORK2] Added working form (#50)

---
+++ 02/Nikita Khadnevich/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Result from './Result';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Result />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
