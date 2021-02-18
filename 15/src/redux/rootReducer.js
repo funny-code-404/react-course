87dc9342aab53c5e949e73e90f35ac160bf267e8
Andy <zfesandydaytona@gmail.com>
2021-02-18 20:35:20 +0300

    Revert "[homework15]addedAOE2"
    This reverts commit 842b826d7fb40461731b18f8dc501fed675cf8e3.

--- 15/src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { fetchDataReducer } from '../ducks/fetchDataReducer/fetchDataReducer';
-import { paginationsReducer } from '../ducks/paginationsReducer';
-
-export const rootReducer = combineReducers({
-    ageOfEmpires: fetchDataReducer,
-    paginations: paginationsReducer
-});
