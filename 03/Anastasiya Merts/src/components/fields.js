74072409686a85f26cc6333ffc6af67a65bb05eb
MertsAnastasiya <73317252+MertsAnastasiya@users.noreply.github.com>
2021-02-22 14:03:39 +0300

    Anastasiya homework3 (#64)
    * Anastasiya added homework 2
    
    * Anastasiya added homework 3
    
    Co-authored-by: Anastasiya <Anastasiya@Sergeys-MacBook-Pro.local>

---
+++ 03/Anastasiya Merts/src/components/fields.js
@@ -0,0 +1,15 @@
+export const FIELDS_NAME = {
+    id: 'N',
+    brand: 'Марка',
+    model: 'Модель',
+    year: 'Год выпуска',
+    price: 'Цена',
+};
+
+export const FIELDS = {
+    id: 'id',
+    brand: 'brand',
+    model: 'model',
+    year: 'year',
+    price: 'price',
+};
