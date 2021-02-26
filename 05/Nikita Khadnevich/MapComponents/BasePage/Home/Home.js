ecd8aa0e29355d055d8322fa301a11f821317f37
NikitaKhadnevich <70269922+NikitaKhadnevich@users.noreply.github.com>
2021-02-26 20:26:03 +0300

    [HOMEWORK5] added Router SPA (#72)

---
+++ 05/Nikita Khadnevich/MapComponents/BasePage/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+
+   render() {
+      return (
+         <div cl>
+            <h3>Главная страница</h3>
+         </div>
+      )
+   }
+}
