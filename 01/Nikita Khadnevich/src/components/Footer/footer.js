a6c6c1da971ac4343431738b935249bcb91c9fe8
NikitaKhadnevich <70269922+NikitaKhadnevich@users.noreply.github.com>
2021-02-28 15:06:46 +0300

    [HOMEWORK1] Fixed components Data

--- 01/Nikita Khadnevich/src/components/Footer/footer.js
+++ 01/Nikita Khadnevich/src/components/Footer/footer.js
@@ -1,7 +1,7 @@
 import './styleFooter.css';
 import Slogan from './../Midblock/Slogan/slogan'
 import Logo from './../Header/Logo/logo'
-export { phoneData, adressData, SocialData } from '../Data/Data'
+import { phoneData, adressData, SocialData } from '../Data/Data'
 
 const Footer = () => {
    return (
@@ -12,15 +12,15 @@ const Footer = () => {
          </div>
          <div className='adressBlock'>
             <Slogan message={adressData.name}/>
-            <Slogan message={adressData.adredss}/>
+            <Slogan message={adressData.adress}/>
          </div>
          <div className='socialBlock'>
             <Logo className='InstBlock'
-            links='https://www.instagram.com/'
-               imgLink='https://w7.pngwing.com/pngs/464/414/png-transparent-instagram-logo-the-factory-kitchen-social-media-facebook-instagram-logo-retail-social-media-marketing-business.png' />
+            links={SocialData.insta}
+               imgLink={SocialData.instaImg} />
             <Logo className='FbBlock'
-            links='https://www.fb.com/'
-               imgLink='https://www.kindpng.com/picc/m/32-326347_youtube-facebook-art-painting-icon-facebook-vector-black.png' /> 
+            links={SocialData.fb}
+               imgLink={SocialData.fbImg} /> 
          </div>
       </div>
    )
