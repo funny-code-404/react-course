805b2e2d6cad663b352033b1f3bba46e1d8435b7
EkaterinaPanchenko <68436058+EkaterinaPanchenko@users.noreply.github.com>
2021-03-22 17:13:43 +0300

    Add homework-11 (#86)
    * Add homework-11
    
    * Code review
    
    * Code review

---
+++ 11/panchenko-ekaterina/src/components/Todos.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { ACTION_GET_DATA } from "../middlewares/actions";
+import { Link } from "react-router-dom";
+
+const TodosList = ({ getData, data, error }) => {
+  useEffect(() => {
+    getData("/todos");
+  }, [getData]);
+
+  return (
+    <section>
+      {data.length ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <Link
+                to={{
+                  pathname: `/todos/${item.id}`,
+                  state: { item },
+                }}
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        error && <h1>{error}</h1>
+      )}
+    </section>
+  );
+};
+
+export const mapStateToProps = ({ todos }) => ({
+  data: todos.data,
+  error: todos.error,
+});
+
+export const mapDispatchToProps = (dispatch) => ({
+  getData: (path) => {
+    dispatch(ACTION_GET_DATA(path));
+  },
+});
+
+export const Todos = connect(mapStateToProps, mapDispatchToProps)(TodosList);
+
+TodosList.propTypes = {
+  getData: PropTypes.func,
+  data: PropTypes.array,
+  error: PropTypes.string,
+};
