72a3f1df0acc07e813a5786066074556986a8a80
pyrus-vagus <grushadaria@gmail.com>
2021-04-07 15:17:25 +0300

    fix: change pictures urls

--- 01/darya-grusha/src/App.js
+++ 01/darya-grusha/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ".components/styles.css";
+import "./components/styles.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 class App extends React.Component {
