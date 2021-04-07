80ad067308cde8432bcdcc6e3735c791a6886a0d
pyrus-vagus <grushadaria@gmail.com>
2021-04-07 15:02:04 +0300

    feat: implement 'Main' block

---
+++ 01/darya-grusha/src/components/Main/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>
+          <span>Food</span> FARMING
+        </h1>
+        <p>
+          Food Farming is proud of our past and excited about our future. We
+          strive to continue being one of India and New Zealand's great food
+          companies, offering quality fruits and vegetable.
+        </p>
+        <a href="#" className="butt">
+          Read More
+        </a>
+        <a href="#" className="mouse"></a>
+      </main>
+    );
+  }
+}
+
+export default Main;
