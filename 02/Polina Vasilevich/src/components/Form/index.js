b9bf5638d5e090a74e1337efd77d261b574b5b6b
Polina Vasilevich <polina.vasilevichh@gmail.com>
2021-04-09 09:57:35 +0300

    added validate form

--- 02/Polina Vasilevich/src/components/Form/index.js
+++ 02/Polina Vasilevich/src/components/Form/index.js
@@ -3,40 +3,109 @@ import "./styles.scss";
 
 class Form extends React.Component {
   state = {
-    date: {},
+    data: {},
     errors: {},
     isValid: true,
   };
+
+  inputs = this.props.inputs;
 
   handleChange = (e) => {
     const { name, value } = e.target;
 
     this.setState((prevState) => ({
       ...prevState,
-      date: { ...prevState.date, [name]: value },
+      data: { ...prevState.data, [name]: value },
     }));
   };
 
-  validate() {}
+  validate() {
+    const { data } = this.state;
+    const errors = {};
+    let isValid = true;
+
+    function validateName(name) {
+      if (!(name.length > 3 && name.length < 30)) {
+        errors["name"] = "The name must be between 3 and 30 characters long.";
+        isValid = false;
+      }
+      return isValid;
+    }
+
+    function validateEmail(email) {
+      if (!String(email).includes("@")) {
+        isValid = false;
+        errors["email"] = "The email must have symbol '@'.";
+      } else if (!String(email).includes(".")) {
+        isValid = false;
+        errors["email"] = "The email must have symbol '.'";
+      }
+
+      return isValid;
+    }
+
+    function validatePassword(password) {
+      if (password < 8) {
+        isValid = false;
+        errors["password"] = "The password must be more 8 symbols.";
+      } else if (password) {
+      }
+
+      return isValid;
+    }
+
+    this.inputs.map(({ name, type }) => {
+      if (!data[name]) {
+        isValid = false;
+        errors[name] = `Please enter ${name}`;
+      } else {
+        switch (type) {
+          case "name":
+            validateName(data[name]);
+          case "email":
+            validateEmail(data[name]);
+          case "password":
+            validatePassword(data[name]);
+        }
+      }
+    });
+
+    this.setState((prevState) => ({
+      ...prevState,
+      errors,
+      isValid,
+    }));
+
+    return isValid;
+  }
 
   handleClick = (e) => {
     e.preventDefault();
-    console.log(this.state.date);
+
+    if (this.validate()) {
+      console.log(this.state.data);
+    }
   };
 
   render() {
     const { inputs } = this.props;
+    const { errors } = this.state;
     return (
       <form className="form" onChange={this.handleChange}>
         <h2 className="form__title">ACCOUNT LOGIN</h2>
         {inputs.map(({ id, type, name, placeholder }) => (
-          <input
-            key={id}
-            type={type}
-            name={name}
-            placeholder={placeholder}
-            className="form__input"
-          />
+          <div className="form__container">
+            <input
+              key={id}
+              type={type}
+              name={name}
+              placeholder={placeholder}
+              className={`form__input ${errors[name] && "notValid"}`}
+            />
+            {errors[name] && (
+              <span className="form__errors">{errors[name]}</span>
+            )}
+          </div>
         ))}
 
         <button
