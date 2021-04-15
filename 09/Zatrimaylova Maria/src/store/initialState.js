a7063fd789f391c8a5e88573e153ae66a8bd925d
Zatrimaylova <m.zatrimaylova@gmail.com>
2021-04-16 00:58:16 +0300

    deleted homework 9

--- 09/Zatrimaylova Maria/src/store/initialState.js
+++
@@ -1,9 +0,0 @@
-import { initialBaseFormState, initialCheckInFormState, initialLogInFormState, initialDeletingFormState } 
-from './rootReducer';
-
-export const initialState = {
-    baseForm: initialBaseFormState,
-    checkInForm: initialCheckInFormState,
-    logInForm: initialLogInFormState,
-    deletingForm: initialDeletingFormState,
-} 
