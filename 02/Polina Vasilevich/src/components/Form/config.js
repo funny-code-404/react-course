e3d877c1cf1231a18f3f2d4e0d12b1e0520a8994
Polina Vasilevich <71717580+PolinaVasilevich@users.noreply.github.com>
2021-04-24 20:54:42 +0300

    2d homework (#117)

---
+++ 02/Polina Vasilevich/src/components/Form/config.js
@@ -0,0 +1,24 @@
+export const config = {
+  inputs: [
+    {
+      id: "01",
+      type: "name",
+      name: "name",
+      placeholder: "Name",
+    },
+
+    {
+      id: "02",
+      type: "email",
+      name: "email",
+      placeholder: "Email",
+    },
+
+    {
+      id: "03",
+      type: "password",
+      name: "password",
+      placeholder: "Password",
+    },
+  ],
+};
