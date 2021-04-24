30ff0b4a849ed83b9c9c094449bfd4e28cb1f5a7
AlexanderChernyak <alexanderchernyak@gmail.com>
2021-04-24 20:07:36 +0300

    - Homework 5 (#137)
    * - homework #1
    
    * - homework 5

---
+++ 05/Aliaksandr Charniak/homework_5/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import ErrorBoundary from './components/ErrorBoundary'
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root')
+)
