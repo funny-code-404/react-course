a7694c1adb6850a8ec6ef2c2dd755e11a51039b9
zatrimaylova <76529464+zatrimaylova@users.noreply.github.com>
2021-04-24 20:49:45 +0300

    Homework 9 (#123)

---
+++ 09/Zatrimaylova Maria/src/components/AccountDeleting/style.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const StyledInput = styled.input`
+    outline: none;
+    width: 300px;
+    height: 30px;
+    margin: 0 auto;
+`;
+
+export const StyledButton = styled.button`
+    width: 150px;
+    height: 30px;
+    outline: none;
+    background: lightgray;
+    border: none;
+    border-radius: 7px;
+    margin: 10px 0 0 150px;
+`;
+
+export const StyledListRate = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 110px;
+    margin-top: 5px;
+`;
+
+export const StyledText = styled.p`
+    margin: 0;
+`;
+
+export const StyledLogInContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 300px;
+`;
+
+export const Styledtextarea = styled.textarea`
+    width: 300px;
+    height: 70px;
+    outline: none;
+    resize: none;
+    margin-bottom: 10px;
+`;
+
+export const StyledCheckIn = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+`;
+
+export const StyledTitle = styled.h3`
+    margin-bottom: 10px;
+`;
+
+export const StyledRadioContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 110px;
+`;
