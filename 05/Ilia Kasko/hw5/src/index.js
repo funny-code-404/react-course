1da0b4adfb60d92f078bfbd16a400d7d951d0432
iliakosko <72011409+iliakosko@users.noreply.github.com>
2021-04-30 09:40:24 +0300

    Hw (#122)

---
+++ 05/Ilia Kasko/hw5/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import ErrorBoundary from "./components/ErrorBoundary";
+
+
+ReactDOM.render(
+    <ErrorBoundary>
+        <App />
+    </ErrorBoundary>,
+  document.getElementById('root')
+);
+
