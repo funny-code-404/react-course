58e2b2f79df50bb89141213aa5d6d2352972337b
Darya Shrubok <55853211+DaryaShr@users.noreply.github.com>
2021-05-12 16:40:28 +0300

    Homework 5 (#141)

---
+++ 05/Darya Shrubok/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+
+  document.getElementById("root")
+);
