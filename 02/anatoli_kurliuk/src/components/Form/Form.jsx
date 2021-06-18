78f22e75f536e74e5c79cef7133307d90e3eafd9
kru4ara <kru4ara@gmail.com>
2021-06-18 17:26:28 +0300

    bbbbb

--- 02/anatoli_kurliuk/src/components/Form/Form.jsx
+++ 02/anatoli_kurliuk/src/components/Form/Form.jsx
@@ -1,73 +1,150 @@
 import React from "react";
-import { header, MAIL, NAME, PASSWORD } from "./config";
 import "./style.css";
+import {
+  HEADER,
+  MAIL,
+  MAIL_TEXT_ERROR,
+  NAME,
+  NAME_TEXT_ERROR,
+  PASSWORD,
+  PASSWORD_TEXT_ERROR,
+  regexp_MAIL,
+  regexp_NAME,
+  regexp_PASSWORD,
+} from "./config";
 
 class Form extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      [NAME]: "",
-      [MAIL]: "",
-      [PASSWORD]: "",
+      name: "",
+      mail: "",
+      password: "",
+      validateName: true,
+      validateMail: true,
+      validatePassword: true,
     };
   }
 
   handleClick = (event) => {
     event.preventDefault();
+    const {
+      name,
+      mail,
+      password,
+      validateName,
+      validateMail,
+      validatePassword,
+    } = this.state;
 
-    const { ...inputs } = this.state;
-
-    console.log({
-      [NAME]: inputs[NAME],
-      [MAIL]: inputs[MAIL],
-      [PASSWORD]: inputs[PASSWORD],
-    });
+    if (validateName && validateMail && validatePassword) {
+      this.setState((prevState) => ({
+        ...prevState,
+        name: "",
+        mail: "",
+        password: "",
+      }));
+    }
   };
 
   handleChange = (event) => {
     const key = event.target.name;
     const inputValue = event.target.value;
-
-    this.setState({
+    this.setState((prevState) => ({
+      ...prevState,
       [key]: inputValue,
-    });
+    }));
+    !this.state.validateName && this.validateName(event);
+    !this.state.validateMail && this.validateMail(event);
+    !this.state.validatePassword && this.validatePassword(event);
+  };
+
+  validateName = (event) => {
+    if (event.target.name === NAME) {
+      this.setState((prevState) => ({
+        ...prevState,
+        validateName: regexp_NAME.test(event.target.value),
+      }));
+    }
+  };
+
+  validateMail = (event) => {
+    if (event.target.name === MAIL) {
+      this.setState((prevState) => ({
+        ...prevState,
+        validateMail: regexp_MAIL.test(event.target.value),
+      }));
+    }
+  };
+
+  validatePassword = (event) => {
+    if (event.target.name === PASSWORD) {
+      this.setState((prevState) => ({
+        ...prevState,
+        validatePassword: regexp_PASSWORD.test(event.target.value),
+      }));
+    }
   };
 
   render() {
-    const { ...values } = this.state;
-    return <form>
-      <h1>{header}</h1>
-      <label>
-        <input
-          name={NAME}
-          placeholder="Name"
-          value={values[NAME]}
-          onChange={this.handleChange}
-        />
-      </label>
+    const {
+      name,
+      mail,
+      password,
+      validateName,
+      validateMail,
+      validatePassword,
+    } = this.state;
+    return (
+      <form>
+        <h1>{HEADER}</h1>
+        <label>
+          <input
+            className={validateName ? "" : "error"}
+            type="text"
+            name={NAME}
+            placeholder="Name"
+            value={name}
+            onChange={this.handleChange}
+            onBlur={this.validateName}
+          />
+          {!validateName && <span className="errorText">{NAME_TEXT_ERROR}</span>}
+        </label>
 
-      <label>
-        <input
-          name={MAIL}
-          placeholder="E-Mail"
-          value={values[MAIL]}
-          onChange={this.handleChange}
-        />
-      </label>
+        <label>
+          <input
+            className={validateMail ? "" : "error"}
+            type="email"
+            name={MAIL}
+            placeholder="E-Mail"
+            value={mail}
+            onChange={this.handleChange}
+            onBlur={this.validateMail}
+          />
+          {!validateMail && (
+            <span className="errorText">{MAIL_TEXT_ERROR}</span>
+          )}
+        </label>
 
-      <label>
-        <input
-          type="password"
-          name={PASSWORD}
-          placeholder="Password"
-          value={values[PASSWORD]}
-          onChange={this.handleChange}
-        />
-      </label>
+        <label>
+          <input
+            className={validatePassword ? "" : "error"}
+            type="password"
+            name={PASSWORD}
+            placeholder="Password"
+            value={password}
+            onChange={this.handleChange}
+            onBlur={this.validatePassword}
+          />
+          {!validatePassword && (
+            <span className="errorText">{PASSWORD_TEXT_ERROR}</span>
+          )}
+        </label>
 
-      <button onClick={this.handleClick}>Ok</button>
-    </form>;
+        <button onClick={this.handleClick}>Send</button>
+      </form>
+    );
   }
 }
 
