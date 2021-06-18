968f806cd765917c9c626f0fa5161387e5aa92f6
Viktoriapola <viktoriapola8@gmail.com>
2021-06-18 16:46:54 +0300

    added homework

---
+++ 02/viktoriapola/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './components/Form/Form'
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Form />
+      </>
+    )
+  }
+}
+
+export default App;
