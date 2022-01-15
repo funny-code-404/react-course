e434302efe7da807318060171f54792252c04676
svetlana-botyan <svetlana.botian@gmail.com>
2022-01-16 01:51:17 +0300

    Add homework1

--- 01/svetlana-botyan/src/App.js
+++ 01/svetlana-botyan/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <Main /> 
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
