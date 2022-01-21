8c677807d88699b4339fd0c73cfb9fb18502a5bb
Dreezar <uladzislau.razhko@gmail.com>
2022-01-21 13:04:23 +0100

    [01] Created homework 1.0

---
+++ 01/Dreezar/lesson-1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
+export default App;
