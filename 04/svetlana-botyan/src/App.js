caa028ed2d878b462d853ba428b099534daa0884
svetlana-botyan <svetlana.botian@gmail.com>
2022-01-31 21:17:08 +0300

    [04] Added homework 4

---
+++ 04/svetlana-botyan/src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-unresolved */
+import List from './componets/List';
+import Form from './componets/Form';
+
+
+function App() {
+  return (
+    <>
+      <Form />
+      <List />
+    </>
+  );
+}
+
+export default App;
