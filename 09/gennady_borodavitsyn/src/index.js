9b9ea7dc6d0c7aa2b4d3452a7c428614fd556919
Henadzi Borodavitsyn (GeDaY) <gedaskyev@gmail.com>
2022-02-17 01:02:32 +0300

    Task 09 is done

--- 09/gennady_borodavitsyn/src/index.js
+++ 09/gennady_borodavitsyn/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
