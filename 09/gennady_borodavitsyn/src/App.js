9f243595e2506b4be98edc42a2b9a38731ad2299
Henadzi Borodavitsyn <88055515+GeDaY@users.noreply.github.com>
2022-02-18 17:05:11 +0300

    09 (#251)
    * Base of app
    
    * Task 09 is done

---
+++ 09/gennady_borodavitsyn/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// COMPONENTS
+import Form from './components/Form/Form.jsx';
+import DataBlock from './components/DataBlock/DataBlock.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <DataBlock />
+    </div>
+  );
+}
+
+export default App;
