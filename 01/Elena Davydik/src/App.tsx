bb1beefbdb71502a4b05a782d8cfef356e9975c0
Elena Davydik <96341674+elena-davydik@users.noreply.github.com>
2022-06-03 20:58:40 +0300

    [01] Complete homework-1 (#271)

---
+++ 01/Elena Davydik/src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+
+export function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
