27a06983127e8082b35565d6ff14748cc7843dea
Natasha <natashapahorukova@gmail.com>
2022-06-04 18:34:28 +0300

    complete hw 2

---
+++ 02/Pahorukova/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Main from "./components/main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
