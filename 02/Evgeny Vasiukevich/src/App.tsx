14e4ba6a6e1059abc14c25f30eacadd5b2903b1e
Evgeny Vasiukevich <94754964+EvgenyWas@users.noreply.github.com>
2022-06-04 11:11:45 +0300

    [02] Evgeny Vasiukevich complete the second homework (#276)

---
+++ 02/Evgeny Vasiukevich/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hotels from './components/HomesGuestsLoves/Hotels';
+
+function App() {
+  return (
+    <div className="App">
+      <Hotels/>
+    </div>
+  );
+}
+
+export default App;
