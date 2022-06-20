a260364c07da31d97df4193d4f36a6559be41238
Davids Kokojevs <96305610+kokoiev34@users.noreply.github.com>
2022-06-20 17:34:10 +0300

    [HW_2] Kokojevs Complete HW_2 (#289)

---
+++ 02/Kokojevs/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "./components/main"
+import Header from "./components/header";
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Main />
+    </>
+  );
+}
+
+export default App;
