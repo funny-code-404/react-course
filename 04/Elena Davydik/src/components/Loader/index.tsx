a15fb80d22361f0dbed0d44aed2f852453e42bcb
Elena Davydik <96341674+elena-davydik@users.noreply.github.com>
2022-06-20 17:33:41 +0300

    Complete homework-4 (#288)

---
+++ 04/Elena Davydik/src/components/Loader/index.tsx
@@ -0,0 +1,10 @@
+export const Loader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__row">
+                <div className="preloader__item"></div>
+                <div className="preloader__item"></div>
+            </div>
+        </div>
+    )
+}
---
+++ 04/Elena Davydik/src/components/TopSection/index.tsx
@@ -0,0 +1,9 @@
+import { HotelsSearchFormList } from "./SearchForm/HotelsSearchFormList"
+
+export function TopSection() {
+    return (
+        <div className="top-section">
+            <HotelsSearchFormList />
+        </div>
+    )
+}
