be5b8de6526b81ca521b3ae7858728de4f1033ec
Evgeny Vasiukevich <94754964+EvgenyWas@users.noreply.github.com>
2022-06-20 22:03:17 +0300

    docs: add docs of the sixth homework (#298)

---
+++ 06/Evgeny Vasiukevich/src/components/UI/Navbar/Navmenu.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import { StyledMenu } from './styles';
+
+const Navmenu = () => {
+    return (
+        <StyledMenu className='nav__menu'>
+            <span><Link to={'/hotels'} className='nav__link nav__link--active'>Stays</Link></span>
+            <span><Link to={'/hotels'} className='nav__link'>Attractions</Link></span>
+        </StyledMenu>
+    );
+};
+
+export default Navmenu;
