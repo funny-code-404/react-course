b5e0f6e5f50716c4407a0439464e28c98bb55452
Nastassia Shkulkova <96378382+AnastasiaShkulkova@users.noreply.github.com>
2022-06-22 23:03:19 +0300

    Update index.tsx

--- 05/shkulkova/src/components/Header/HotelParams/index.tsx
+++ 05/shkulkova/src/components/Header/HotelParams/index.tsx
@@ -6,6 +6,30 @@ export const HotelParams = () => {
   const [children, setChildren] = useState(0);
   const [rooms, setRooms] = useState(1);
 
+  const minusAdult = () => {
+    setAdults(adults - 1);
+  };
+
+  const plusAdult = () => {
+    setAdults(adults + 1);
+  };
+
+  const minusChild = () => {
+    setChildren(children - 1);
+  };
+
+  const plusChild = () => {
+    setChildren(children + 1);
+  };
+
+  const minusRoom = () => {
+    setRooms(rooms - 1);
+  };
+
+  const plusRoom = () => {
+    setRooms(rooms + 1);
+  };
+
   return (
     <>
       <button className="params_button" onClick={() => setOpened(!opened)}>
@@ -16,17 +40,11 @@ export const HotelParams = () => {
           <div className="params_item">
             <p className="params_item_title">Adults</p>
             <div className="item_counter">
-              <button
-                className="params_item_btn"
-                onClick={() => setAdults(adults - 1)}
-              >
+              <button className="params_item_btn" onClick={minusAdult}>
                 -
               </button>
               <p className="params_item_number">{adults}</p>
-              <button
-                className="params_item_btn"
-                onClick={() => setAdults(adults + 1)}
-              >
+              <button className="params_item_btn" onClick={plusAdult}>
                 +
               </button>
             </div>
@@ -34,17 +52,11 @@ export const HotelParams = () => {
           <div className="params_item">
             <p className="params_item_title">Children</p>
             <div className="item_counter">
-              <button
-                className="params_item_btn"
-                onClick={() => setChildren(children - 1)}
-              >
+              <button className="params_item_btn" onClick={minusChild}>
                 -
               </button>
               <p className="params_item_number">{children}</p>
-              <button
-                className="params_item_btn"
-                onClick={() => setChildren(children + 1)}
-              >
+              <button className="params_item_btn" onClick={plusChild}>
                 +
               </button>
             </div>
@@ -52,17 +64,11 @@ export const HotelParams = () => {
           <div className="params_item">
             <p className="params_item_title">Rooms</p>
             <div className="item_counter">
-              <button
-                className="params_item_btn"
-                onClick={() => setRooms(rooms - 1)}
-              >
+              <button className="params_item_btn" onClick={minusRoom}>
                 -
               </button>
               <p className="params_item_number">{rooms}</p>
-              <button
-                className="params_item_btn"
-                onClick={() => setRooms(rooms + 1)}
-              >
+              <button className="params_item_btn" onClick={plusRoom}>
                 +
               </button>
             </div>
