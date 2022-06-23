47591d073be461d382913c4df8de1d29ae0f2fd2
Nastya <96421604+kameiko-n@users.noreply.github.com>
2022-06-23 16:33:00 +0300

    hw-3

---
+++ 03/Kameiko Nastya/src/App.tsx
@@ -0,0 +1,13 @@
+import {Search}   from './components/search';
+
+import './index.scss'
+
+function App() {
+  return (
+    <div className="App">
+     <Search />
+    </div>
+  );
+}
+
+export default App;
