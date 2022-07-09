3f9614adb11861321d7ae0ef8149e71d2df4efaa
Nastassia Shkulkova <96378382+AnastasiaShkulkova@users.noreply.github.com>
2022-07-09 17:41:23 +0300

    Update types.ts

--- 09/shkulkova/src/components/redux/headerSerchSection/types.ts
+++ 09/shkulkova/src/components/redux/headerSerchSection/types.ts
@@ -8,11 +8,7 @@ export type RoomsType = { rooms: number };
 export type SearchSectionProperties = AdultsType & ChildrenType & RoomsType;
 export type DateType = Date | null;
 
-
-
-
 export type SearchSectionProperty = AdultsType | ChildrenType | RoomsType;
-
 
 export type CounterState = {
     isModalOpen: boolean,
@@ -30,10 +26,6 @@ export type SearchState = {
   rooms: number,
   dateFrom: DateType,
   dateTo: DateType,
-  // counter: {
-  //   properties: SearchSectionProperties,
-  //   states: CounterState,
-  // }
 };
 
 
